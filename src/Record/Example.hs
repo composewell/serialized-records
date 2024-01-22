c46adff96a3eb6ac55c7af42407ac4518859bb3d
Adithya Kumar <adithya@composewell.com>
2024-01-22 20:47:33 +0530

    Implement all the instances

--- src/Record/Example.hs
+++ src/Record/Example.hs
@@ -4,7 +4,10 @@
 module Record.Example where
 
 import Record.Types
+
+import Data.Maybe (fromJust)
 import Data.Proxy (Proxy(..))
+
 import qualified Streamly.Internal.Data.Array as Array
 
 --------------------------------------------------------------------------------
@@ -19,30 +22,29 @@ data Address = Address
 -- This should be derived via template-haskell helpers
 instance RecordMeta (Record Address) where
     --   2
-    -- + 2 + length "zipCode"
-    -- + 2 + length "country"
-    headerLength _ = 18
+    -- + 2 + length "zipCode" + 4 -- 13
+    -- + 2 + length "country" + 4 -- 13
+    -- headerLength _ = 28
+
     -- TODO: Use xxHash here and use a builder like combination
     typeHash _ = Array.getSliceUnsafe 0 32 $
-           encodeSimpleString "zipCode" <> recPrimHash (valueType (Proxy :: Proxy Int))
-        <> encodeSimpleString "country" <> recPrimHash (valueType (Proxy :: Proxy String))
+           encodeSimpleString "zipCode" <> recPrimHash (toValueProxy (Proxy :: Proxy Int))
+        <> encodeSimpleString "country" <> recPrimHash (toValueProxy (Proxy :: Proxy String))
         <> Array.fromList (replicate 32 0)
 
 instance RecordMeta (Record Address) => HasField (Proxy "zipCode") (Record Address) Int where
 
-    fieldPosition :: Proxy "zipCode" -> Record Address -> FieldPos
-    fieldPosition _ _ = ICValue (offsetMessageBody (headerLength (Proxy :: Proxy (Record Address))))
-
     getField :: Proxy "zipCode" -> Record Address -> Int
-    getField _ _ = error "Unimplemented"
+    getField _ (Record True arr) = getFieldTrustedStatic (offsetMessageBody 28) arr
+    getField _ (Record False arr) =
+        fromJust $ getFieldUntrusted 28 (encodeSimpleString "zipCode") arr
 
 instance RecordMeta (Record Address) => HasField (Proxy "country") (Record Address) String where
 
-    fieldPosition :: Proxy "country" -> Record Address -> FieldPos
-    fieldPosition _ _ = ICValue (offsetMessageBody (headerLength (Proxy :: Proxy (Record Address)) + 8))
-
     getField :: Proxy "country" -> Record Address -> String
-    getField _ _ = error "Unimplemented"
+    getField _ (Record True arr) = getFieldTrustedStatic ((offsetMessageBody 28) + 8) arr
+    getField _ (Record False arr) =
+        fromJust $ getFieldUntrusted 28 (encodeSimpleString "country") arr
 
 --------------------------------------------------------------------------------
 -- User
@@ -60,60 +62,56 @@ data User = User
 -- order: [age, height, isMarried, name, address]
 instance RecordMeta (Record User) where
     --   2
-    -- + 2 + length "name"      -- 6
-    -- + 2 + length "age"       -- 5
-    -- + 2 + length "height"    -- 8
-    -- + 2 + length "isMarried" -- 11
-    -- + 2 + length "address"   -- 9
-    headerLength _ = 41
+    -- + 2 + length "name"      + 4 -- 10
+    -- + 2 + length "age"       + 4 -- 9
+    -- + 2 + length "height"    + 4 -- 12
+    -- + 2 + length "isMarried" + 4 -- 15
+    -- + 2 + length "address"   + 4 -- 13
+    -- headerLength _ = 61
+
     -- TODO: Use xxHash here and use a builder like combination
     typeHash _ = Array.getSliceUnsafe 0 32 $
-           encodeSimpleString "name" <> recPrimHash (valueType (Proxy :: Proxy String))
-        <> encodeSimpleString "age" <> recPrimHash (valueType (Proxy :: Proxy Int))
-        <> encodeSimpleString "height" <> recPrimHash (valueType (Proxy :: Proxy Double))
-        <> encodeSimpleString "isMarried" <> recPrimHash (valueType (Proxy :: Proxy Bool))
-        <> encodeSimpleString "address" <> recPrimHash (valueType (Proxy :: Proxy (Record Address)))
+           encodeSimpleString "name" <> recPrimHash (toValueProxy (Proxy :: Proxy String))
+        <> encodeSimpleString "age" <> recPrimHash (toValueProxy (Proxy :: Proxy Int))
+        <> encodeSimpleString "height" <> recPrimHash (toValueProxy (Proxy :: Proxy Double))
+        <> encodeSimpleString "isMarried" <> recPrimHash (toValueProxy (Proxy :: Proxy Bool))
+        <> encodeSimpleString "address" <> recPrimHash (toValueProxy (Proxy :: Proxy (Record Address)))
         <> Array.fromList (replicate 32 0)
 
 instance HasField (Proxy "age") (Record User) Int where
 
-    fieldPosition :: Proxy "age" -> Record User -> FieldPos
-    fieldPosition _ _ = ICValue (offsetMessageBody (headerLength (Proxy :: Proxy (Record User))))
-
     getField :: Proxy "age" -> Record User -> Int
-    getField _ _ = error "Unimplemented"
+    getField _ (Record True arr) = getFieldTrustedStatic (offsetMessageBody 61) arr
+    getField _ (Record False arr) =
+        fromJust $ getFieldUntrusted 28 (encodeSimpleString "age") arr
 
 instance HasField (Proxy "height") (Record User) Double where
 
-    fieldPosition :: Proxy "height" -> Record User -> FieldPos
-    fieldPosition _ _ = ICValue (offsetMessageBody (headerLength (Proxy :: Proxy (Record User)) + 8))
-
     getField :: Proxy "height" -> Record User -> Double
-    getField _ _ = error "Unimplemented"
+    getField _ (Record True arr) = getFieldTrustedStatic ((offsetMessageBody 61) + 8) arr
+    getField _ (Record False arr) =
+        fromJust $ getFieldUntrusted 28 (encodeSimpleString "height") arr
 
 instance HasField (Proxy "isMarried") (Record User) Bool where
 
-    fieldPosition :: Proxy "isMarried" -> Record User -> FieldPos
-    fieldPosition _ _ = ICValue (offsetMessageBody (headerLength (Proxy :: Proxy (Record User)) + 12))
-
     getField :: Proxy "isMarried" -> Record User -> Bool
-    getField _ _ = error "Unimplemented"
+    getField _ (Record True arr) = getFieldTrustedStatic ((offsetMessageBody 61) + 16) arr
+    getField _ (Record False arr) =
+        fromJust $ getFieldUntrusted 28 (encodeSimpleString "isMarried") arr
 
 instance HasField (Proxy "name") (Record User) String where
 
-    fieldPosition :: Proxy "name" -> Record User -> FieldPos
-    fieldPosition _ _ = ICValue (offsetMessageBody (headerLength (Proxy :: Proxy (Record User)) + 13))
-
     getField :: Proxy "name" -> Record User -> String
-    getField _ _ = error "Unimplemented"
+    getField _ (Record True arr) = getFieldTrustedStatic ((offsetMessageBody 61) + 17) arr
+    getField _ (Record False arr) =
+        fromJust $ getFieldUntrusted 28 (encodeSimpleString "name") arr
 
 instance HasField (Proxy "address") (Record User) (Record Address) where
 
-    fieldPosition :: Proxy "address" -> Record User -> FieldPos
-    fieldPosition _ _ = ICOffsetToValue (offsetHeaderBody + 30)
-
     getField :: Proxy "address" -> Record User -> (Record Address)
-    getField _ _ = error "Unimplemented"
+    getField _ (Record True arr) = getFieldTrustedDynamic (offsetHeaderBody + 55) arr
+    getField _ (Record False arr) =
+        fromJust $ getFieldUntrusted 28 (encodeSimpleString "address") arr
 
 --------------------------------------------------------------------------------
 -- Example
--- src/Record/Types.hs
+++ src/Record/Types.hs
@@ -19,17 +19,15 @@ import Data.Int (Int16, Int32, Int64)
 import Data.Proxy (Proxy(..))
 import Data.Word (Word8)
 import Streamly.Data.Array (Array)
-import Streamly.Data.MutArray (MutArray)
 import Streamly.Internal.System.IO (unsafeInlineIO)
-import Streamly.Internal.Data.MutByteArray (Serialize)
+import Streamly.Internal.Data.MutByteArray (MutByteArray)
 import System.IO.Unsafe (unsafePerformIO)
+import Streamly.Internal.Data.Array (Array(..))
 
 import qualified Streamly.Data.Stream as Stream
 import qualified Streamly.Unicode.Stream as Encoding
 import qualified Streamly.Internal.Data.Array as Array
 import qualified Streamly.Internal.Data.MutByteArray as Serialize
-
-#include "MachDeps.h"
 
 --------------------------------------------------------------------------------
 -- Record Spec
@@ -54,7 +52,7 @@ offsetHeaderBody :: Int
 offsetHeaderBody = 40
 
 offsetMessageBody :: Int -> Int
-offsetMessageBody headerBodyLen = offsetHeaderBody + headerBodyLen
+offsetMessageBody headerLen = offsetHeaderBody + headerLen
 
 getEncodedTypeHash :: Array Word8 -> Array Word8
 getEncodedTypeHash = Array.getSliceUnsafe offsetTypeHash (offsetTypeHash + 32)
@@ -73,55 +71,85 @@ newtype Utf8 = Utf8 (Array Word8)
 -- Should we use MutByteArray here?
 class IsRecordPrimitive a where
     recPrimHash :: Proxy a -> Array Word8
-    recPrimSerializeAt :: Int -> MutArray Word8 -> a -> IO Int
-    recPrimDeserializeAt :: Int -> MutArray Word8 -> IO (a, Int)
+    recPrimAddSizeTo :: Int -> a -> Int
+    recPrimSerializeAt :: Int -> MutByteArray -> a -> IO Int
+    recPrimDeserializeAt :: Int -> MutByteArray -> Int -> IO (Int, a)
 
 class IsRecordPrimitive b => ValueMapper a b | a -> b where
-    valueType :: Proxy a -> Proxy b
-    valueType _ = Proxy
+    toValueProxy :: Proxy a -> Proxy b
+    toValueProxy _ = Proxy
     toValue :: a -> b
     fromValue :: b -> a
 
-data FieldPos
-    = ICOffsetToValue Int
-    | ICValue Int
-
 class HasField k r v | k r -> v where
     getField :: k -> r -> v
-    fieldPosition :: k -> r -> FieldPos
 
 class RecordMeta k where
     typeHash :: Proxy k -> Array Word8
-    headerLength :: Proxy k -> Int
 
 --------------------------------------------------------------------------------
 -- Instances
 --------------------------------------------------------------------------------
 
-instance RecordMeta (Record a) => IsRecordPrimitive (Record a) where
+instance forall a. RecordMeta (Record a) => IsRecordPrimitive (Record a) where
     recPrimHash = typeHash
-    recPrimSerializeAt = undefined
-    recPrimDeserializeAt = undefined
+    recPrimAddSizeTo i (Record _ arr) = i + 4 + Array.length arr
+    recPrimSerializeAt i target (Record _ (Array src start end)) = do
+        let arrLen = end - start
+        i1 <- Serialize.serializeAt i target (i_i32 arrLen)
+        Serialize.putSliceUnsafe src start target i1 arrLen
+        pure (i1 + arrLen)
+    recPrimDeserializeAt i arr end = do
+        (i1, len32) <- Serialize.deserializeAt i arr end :: IO (Int, Int32)
+        let len = i32_i len32
+            record = Array arr i1 (i1 + len)
+            encodedTypeHash = getEncodedTypeHash record
+            typeMatch = typeHash (Proxy :: Proxy (Record a)) == encodedTypeHash
+        pure $ (4 + len, Record typeMatch record)
 
 instance IsRecordPrimitive Utf8 where
     recPrimHash _ = Array.getSliceUnsafe 0 1 arr0To9
-    recPrimSerializeAt = undefined
-    recPrimDeserializeAt = undefined
+    recPrimAddSizeTo i (Utf8 arr) = i + 4 + Array.length arr
+    recPrimSerializeAt i target (Utf8 (Array src start end)) = do
+        let arrLen = end - start
+        i1 <- Serialize.serializeAt i target (i_i32 arrLen)
+        Serialize.putSliceUnsafe src start target i1 arrLen
+        pure (i1 + arrLen)
+    recPrimDeserializeAt i arr end = do
+        (i1, len32) <- Serialize.deserializeAt i arr end :: IO (Int, Int32)
+        let len = i32_i len32
+            utf8 = Array arr i1 (i1 + len)
+        pure $ (4 + len, Utf8 utf8)
 
 instance IsRecordPrimitive Int64 where
     recPrimHash _ = Array.getSliceUnsafe 1 2 arr0To9
-    recPrimSerializeAt = undefined
-    recPrimDeserializeAt = undefined
+    recPrimAddSizeTo = Serialize.addSizeTo
+    recPrimSerializeAt = Serialize.serializeAt
+    recPrimDeserializeAt = Serialize.deserializeAt
 
 instance IsRecordPrimitive Double where
     recPrimHash _ = Array.getSliceUnsafe 2 3 arr0To9
-    recPrimSerializeAt = undefined
-    recPrimDeserializeAt = undefined
+    recPrimAddSizeTo = Serialize.addSizeTo
+    recPrimSerializeAt = Serialize.serializeAt
+    recPrimDeserializeAt = Serialize.deserializeAt
 
 instance IsRecordPrimitive Bool where
     recPrimHash _ = Array.getSliceUnsafe 3 4 arr0To9
-    recPrimSerializeAt = undefined
-    recPrimDeserializeAt = undefined
+    recPrimAddSizeTo = Serialize.addSizeTo
+    recPrimSerializeAt = Serialize.serializeAt
+    recPrimDeserializeAt = Serialize.deserializeAt
+
+instance IsRecordPrimitive Int32 where
+    recPrimHash _ = Array.getSliceUnsafe 5 6 arr0To9
+    recPrimAddSizeTo = Serialize.addSizeTo
+    recPrimSerializeAt = Serialize.serializeAt
+    recPrimDeserializeAt = Serialize.deserializeAt
+
+instance IsRecordPrimitive Int16 where
+    recPrimHash _ = Array.getSliceUnsafe 6 7 arr0To9
+    recPrimAddSizeTo = Serialize.addSizeTo
+    recPrimSerializeAt = Serialize.serializeAt
+    recPrimDeserializeAt = Serialize.deserializeAt
 
 instance ValueMapper Utf8 Utf8 where
     toValue = id
@@ -144,8 +172,18 @@ instance ValueMapper Double Double where
     fromValue = id
 
 instance ValueMapper String Utf8 where
-    toValue = undefined
-    fromValue = undefined
+    toValue =
+        Utf8
+            . unsafePerformIO
+            . Array.fromStream
+            . Encoding.encodeUtf8
+            . Stream.fromList
+    fromValue (Utf8 arr) =
+        unsafeInlineIO
+            $ Stream.toList
+            $ Encoding.decodeUtf8Chunks
+            $ Stream.fromList [arr]
+
 
 --------------------------------------------------------------------------------
 -- Helpers
@@ -166,46 +204,74 @@ arr0To9 = encodeSimpleString "0123456789"
 i32_i :: Int32 -> Int
 i32_i = fromIntegral
 
+{-# INLINE i_i32 #-}
+i_i32 :: Int -> Int32
+i_i32 = fromIntegral
+
 {-# INLINE i16_i #-}
 i16_i :: Int16 -> Int
 i16_i = fromIntegral
 
 {-# INLINE deserializeAt_ #-}
-deserializeAt_ :: Serialize a => Int -> Array Word8 -> IO a
+deserializeAt_ :: IsRecordPrimitive a => Int -> Array Word8 -> IO a
 deserializeAt_ i arr =
     fmap snd
-        $ Serialize.deserializeAt i
+        $ recPrimDeserializeAt i
               (Array.arrContents arr)
               (Array.arrStart arr + Array.length arr)
 
 findFieldIndex :: Int -> Array Word8 -> Int -> Array Word8 -> IO Int32
 findFieldIndex headerLen _ ix _ | ix >= offsetMessageBody headerLen = pure 0
-findFieldIndex headerLen targerKey ix recArr = do
+findFieldIndex headerLen targetKey ix recArr = do
     curKeyLen <- i16_i <$> deserializeAt_ ix recArr
     let curKey = Array.getSliceUnsafe (ix + 2) curKeyLen recArr
-    if targerKey == curKey
+    if targetKey == curKey
     then deserializeAt_ (ix + 2 + curKeyLen) recArr
-    else findFieldIndex headerLen targerKey (ix + 2 + curKeyLen + 4) recArr
+    else findFieldIndex headerLen targetKey (ix + 2 + curKeyLen + 4) recArr
 
-{-# INLINE getFieldGeneric #-}
-getFieldGeneric
-    :: Serialize b
-    => Array Word8
-    -> Int
-    -> FieldPos
+{-# INLINE getFieldTrustedStatic #-}
+getFieldTrustedStatic
+    :: (IsRecordPrimitive a, ValueMapper b a)
+    => Int
+    -> Array Word8
+    -> b
+getFieldTrustedStatic ix recArr =
+    unsafeInlineIO $ fromValue <$> deserializeAt_ ix recArr
+
+{-# INLINE getFieldTrustedDynamic #-}
+getFieldTrustedDynamic
+    :: (IsRecordPrimitive a, ValueMapper b a)
+    => Int
+    -> Array Word8
+    -> b
+getFieldTrustedDynamic ix recArr =
+    unsafeInlineIO $ do
+        ix1 <- i32_i <$> deserializeAt_ ix recArr
+        fromValue <$> deserializeAt_ ix1 recArr
+
+{-# INLINE getFieldTrustedDynamicNullable #-}
+getFieldTrustedDynamicNullable
+    :: (IsRecordPrimitive a, ValueMapper b a)
+    => Int
+    -> Array Word8
+    -> Maybe b
+getFieldTrustedDynamicNullable ix recArr =
+    unsafeInlineIO $ do
+        ix1 <- i32_i <$> deserializeAt_ ix recArr
+        if ix1 == 0
+        then pure Nothing
+        else Just . fromValue <$> deserializeAt_ ix1 recArr
+
+{-# INLINE getFieldUntrusted #-}
+getFieldUntrusted
+    :: (IsRecordPrimitive a, ValueMapper b a)
+    => Int
     -> Array Word8
     -> Array Word8
     -> Maybe b
-getFieldGeneric _ _ (ICValue ix) _ recArr =
-    unsafeInlineIO $ Just <$> deserializeAt_ ix recArr
-getFieldGeneric typHash headerLen (ICOffsetToValue ix) key recArr =
+getFieldUntrusted headerLen key recArr =
     unsafeInlineIO $ do
-        let encodedTypeHash = getEncodedTypeHash recArr
-        fieldIndex <-
-            i32_i <$>
-                if typHash == encodedTypeHash
-                then deserializeAt_ ix recArr  :: IO Int32
-                else findFieldIndex headerLen key 40 recArr
-        if fieldIndex > 0
-        then Just <$> deserializeAt_ fieldIndex recArr
-        else pure Nothing
+        ix <- i32_i <$> findFieldIndex headerLen key 40 recArr
+        if ix == 0
+        then pure Nothing
+        else Just . fromValue <$> deserializeAt_ ix recArr
