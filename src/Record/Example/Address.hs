197e4a6e86c0579d142630b43a2f3dc7a7d07804
Adithya Kumar <adithya@composewell.com>
2024-01-25 17:58:44 +0530

    Use the TH combinators in the example

---
+++ src/Record/Example/Address.hs
@@ -0,0 +1,91 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Record.Example.Address where
+
+import Record.Types
+import Record.TH
+
+import Data.Maybe (fromJust)
+import Data.Proxy (Proxy(..))
+
+#define DERIVE_TH(x) $(decsIsRecordableInstance $(expTypeHash $(expRecordMeta ''x) ''x) $(expRecordMeta ''x) ''x)
+
+--------------------------------------------------------------------------------
+-- Address
+--------------------------------------------------------------------------------
+
+data Address = Address
+    { zipCode :: Int
+    , country :: Maybe (Maybe (Maybe String))
+    }
+
+DERIVE_TH(Address)
+
+{-
+-- This should be derived via template-haskell helpers
+instance IsRecordable Address where
+    --   2 + length "zipCode" + 4 -- 13
+    -- + 2 + length "country" + 4 -- 13
+    -- headerLength _ = 26
+
+    -- TODO: Use xxHash here and use a builder like combination
+    typeHash _ = Array.getSliceUnsafe 0 32 $
+           encodeSimpleString "zipCode" <> recPrimHash (toValueProxy (Proxy :: Proxy Int))
+        <> encodeSimpleString "country" <> recPrimHash (toValueProxy (Proxy :: Proxy String))
+        <> Array.fromList (replicate 32 0)
+
+    recStaticSize _ = Nothing
+
+    createRecord addr = unsafePerformIO $ do
+        let zipCode_ = toValue $ zipCode addr
+            country_ =
+                toValue $ flattenNullable $ flattenNullable $ country addr
+            size =
+                recPrimAddSizeTo (recPrimAddSizeTo 0 zipCode_) country_
+                    + lenVersion
+                    + lenMessageLen
+                    + lenTypeHash
+                    + lenHeaderLen
+                    + 26 -- headerLength
+        arr <- Serialize.new size
+        let i0 = 0
+        i1 <- recPrimSerializeAt i0 arr (0 :: Int16)
+        i2 <- recPrimSerializeAt i1 arr (i_i32 size)
+        let hash = typeHash (Proxy :: Proxy Address)
+        i3 <- unsafePutCompleteSlice i2 arr hash
+        i4 <- recPrimSerializeAt i3 arr (26 :: Int16)
+        (i5, i6) <- unsafePutHeaderKey i4 arr "zipCode"
+        (i7, i8) <- unsafePutHeaderKey i6 arr "country"
+        void $ recPrimSerializeAt i5 arr (i_i32 i8 :: Int32)
+        i9 <- recPrimSerializeAt i8 arr zipCode_
+
+        -- Special handling of Nullable values
+        i10 <- case country_ of
+            Nothing -> do
+                void $ recPrimSerializeAt i7 arr (0 :: Int32)
+                pure i9
+            Just val -> do
+                void $ recPrimSerializeAt i7 arr (i_i32 i9 :: Int32)
+                recPrimSerializeAt i9 arr val
+        pure $ Record True $ Array arr 0 i10
+-}
+
+instance HasField (Proxy "zipCode") (Record Address) Int where
+
+    getField :: Proxy "zipCode" -> Record Address -> Int
+    getField _ (Record True arr) = getFieldTrustedStatic (offsetMessageBody 26) arr
+    getField _ (Record False arr) =
+        fromJust $ getFieldUntrusted 26 (encodeSimpleString "zipCode") arr
+
+instance HasField (Proxy "country") (Record Address) (Maybe (Maybe (Maybe String))) where
+
+    getField :: Proxy "country" -> Record Address -> Maybe (Maybe (Maybe String))
+    getField _ (Record True arr) =
+        case getFieldTrustedNullable (offsetHeaderBody + 22) arr of
+            Nothing -> Nothing
+            Just res -> Just $ Just $ Just res
+    getField _ (Record False arr) = do
+        -- Special handling of Nullable values
+        case getFieldUntrusted 26 (encodeSimpleString "country") arr of
+            Nothing -> Nothing
+            Just res -> Just $ Just $ Just res
---
+++ src/Record/Example/User.hs
@@ -0,0 +1,129 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Record.Example.User where
+
+import Record.Example.Address
+import Record.Types
+import Record.TH
+
+import Data.Maybe (fromJust)
+import Data.Proxy (Proxy(..))
+
+#define DERIVE_TH(x) $(decsIsRecordableInstance $(expTypeHash $(expRecordMeta ''x) ''x) $(expRecordMeta ''x) ''x)
+
+--------------------------------------------------------------------------------
+-- User
+--------------------------------------------------------------------------------
+
+data User = User
+    { name :: String
+    , age :: Int
+    , height :: Double
+    , isMarried :: Bool
+    , address :: Record Address
+    }
+
+DERIVE_TH(User)
+
+{-
+-- TODO: order is decided by the TH combinators
+-- order: [age, height, isMarried, name, address]
+instance IsRecordable User where
+    --   2 + length "name"      + 4 -- 10
+    -- + 2 + length "age"       + 4 -- 9
+    -- + 2 + length "height"    + 4 -- 12
+    -- + 2 + length "isMarried" + 4 -- 15
+    -- + 2 + length "address"   + 4 -- 13
+    -- headerLength _ = 59
+
+    -- TODO: Use xxHash here and use a builder like combination
+    typeHash _ = Array.getSliceUnsafe 0 32 $
+        encodeSimpleString "age" <> recPrimHash (toValueProxy (Proxy :: Proxy Int))
+        <> encodeSimpleString "height" <> recPrimHash (toValueProxy (Proxy :: Proxy Double))
+        <> encodeSimpleString "isMarried" <> recPrimHash (toValueProxy (Proxy :: Proxy Bool))
+        <> encodeSimpleString "address" <> recPrimHash (toValueProxy (Proxy :: Proxy (Record Address)))
+        <> encodeSimpleString "name" <> recPrimHash (toValueProxy (Proxy :: Proxy String))
+        <> Array.fromList (replicate 32 0)
+
+    recStaticSize _ = Nothing
+
+    createRecord user = unsafePerformIO $ do
+        let name_ = toValue $ name user
+            age_ = toValue $ age user
+            height_ = toValue $ height user
+            isMarried_ = toValue $ isMarried user
+            address_ = toValue $ address user
+            size =
+                recPrimAddSizeTo
+                    (recPrimAddSizeTo
+                         (recPrimAddSizeTo
+                              (recPrimAddSizeTo
+                                   (recPrimAddSizeTo 0 address_)
+                                   isMarried_)
+                              height_)
+                         age_)
+                    name_
+                    + lenVersion
+                    + lenMessageLen
+                    + lenTypeHash
+                    + lenHeaderLen
+                    + 59 -- headerLength
+        arr <- Serialize.new size
+        let i0 = 0
+        i1 <- recPrimSerializeAt i0 arr (0 :: Int16)
+        i2 <- recPrimSerializeAt i1 arr (i_i32 size)
+        let hash = typeHash (Proxy :: Proxy User)
+        i3 <- unsafePutCompleteSlice i2 arr hash
+        i4 <- recPrimSerializeAt i3 arr (59 :: Int16)
+        (i5, i6) <- unsafePutHeaderKey i4 arr "age"
+        (i7, i8) <- unsafePutHeaderKey i6 arr "height"
+        (i9, i10) <- unsafePutHeaderKey i8 arr "isMarried"
+        (i11, i12) <- unsafePutHeaderKey i10 arr "address"
+        (i13, i14) <- unsafePutHeaderKey i12 arr "name"
+        void $ recPrimSerializeAt i5 arr (i_i32 i14 :: Int32)
+        i15 <- recPrimSerializeAt i14 arr age_
+        void $ recPrimSerializeAt i7 arr (i_i32 i15 :: Int32)
+        i16 <- recPrimSerializeAt i15 arr height_
+        void $ recPrimSerializeAt i9 arr (i_i32 i16 :: Int32)
+        i17 <- recPrimSerializeAt i16 arr isMarried_
+        void $ recPrimSerializeAt i11 arr (i_i32 i17 :: Int32)
+        i18 <- recPrimSerializeAt i17 arr address_
+        void $ recPrimSerializeAt i13 arr (i_i32 i18 :: Int32)
+        i19 <- recPrimSerializeAt i18 arr name_
+        pure $ Record True $ Array arr 0 i19
+-}
+
+instance HasField (Proxy "age") (Record User) Int where
+
+    getField :: Proxy "age" -> Record User -> Int
+    getField _ (Record True arr) = getFieldTrustedStatic (offsetMessageBody 59) arr
+    getField _ (Record False arr) =
+        fromJust $ getFieldUntrusted 59 (encodeSimpleString "age") arr
+
+instance HasField (Proxy "height") (Record User) Double where
+
+    getField :: Proxy "height" -> Record User -> Double
+    getField _ (Record True arr) = getFieldTrustedStatic ((offsetMessageBody 59) + 8) arr
+    getField _ (Record False arr) =
+        fromJust $ getFieldUntrusted 59 (encodeSimpleString "height") arr
+
+instance HasField (Proxy "isMarried") (Record User) Bool where
+
+    getField :: Proxy "isMarried" -> Record User -> Bool
+    getField _ (Record True arr) = getFieldTrustedStatic ((offsetMessageBody 59) + 16) arr
+    getField _ (Record False arr) =
+        fromJust $ getFieldUntrusted 59 (encodeSimpleString "isMarried") arr
+
+instance HasField (Proxy "name") (Record User) String where
+
+    getField :: Proxy "name" -> Record User -> String
+    getField _ (Record True arr) = getFieldTrustedDynamic (offsetHeaderBody + 55) arr
+    getField _ (Record False arr) =
+        fromJust $ getFieldUntrusted 59 (encodeSimpleString "name") arr
+
+instance HasField (Proxy "address") (Record User) (Record Address) where
+
+    getField :: Proxy "address" -> Record User -> (Record Address)
+    getField _ (Record True arr) = getFieldTrustedStatic ((offsetMessageBody 59) + 17) arr
+    getField _ (Record False arr) =
+        fromJust $ getFieldUntrusted 59 (encodeSimpleString "address") arr
